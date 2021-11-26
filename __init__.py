ffdab26a91e37cb0055e99dce09dffe5dca311ed
YYR <315629555@qq.com>
2021-11-26 18:15:31 +0800

    feat: add blueprint and koh-scoreboard route

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from __future__ import division  # Use floating point for math calculations
 
+from flask import render_template, Blueprint
+
 from CTFd.plugins import register_plugin_assets_directory
 from CTFd.plugins.challenges import CHALLENGE_CLASSES
 from CTFd.plugins.migrations import upgrade
@@ -15,3 +17,16 @@ def load(app):
     register_plugin_assets_directory(
         app, base_path="/plugins/CTFd-KoH/assets/"
     )
+
+    koh_blueprint = Blueprint(
+        "koh",
+        __name__,
+        template_folder="templates",
+        static_folder="assets",
+    )
+
+    @koh_blueprint.route("/koh-scoreboard")
+    def koh_scoreboard():
+        return render_template('koh-scoreboard.html')
+
+    app.register_blueprint(koh_blueprint)

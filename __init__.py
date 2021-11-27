80b512e6595b188bc16a610fcc0e381db54fab38
Konano <nanoapezlk@gmail.com>
2021-11-27 14:40:05 +0800

    feat: add modifier

--- __init__.py
+++ __init__.py
@@ -1,13 +1,27 @@
 from __future__ import division  # Use floating point for math calculations
 
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, request, url_for
 
+from CTFd.constants.config import ChallengeVisibilityTypes, Configs
 from CTFd.plugins import register_plugin_assets_directory, register_user_page_menu_bar
 from CTFd.plugins.challenges import CHALLENGE_CLASSES
 from CTFd.plugins.migrations import upgrade
 from CTFd.api import CTFd_API_v1
-from CTFd.utils.helpers import get_infos
-from CTFd.utils.logging import log
+from CTFd.utils import config
+from CTFd.utils.config import is_teams_mode
+from CTFd.utils.config.visibility import scores_visible
+from CTFd.utils.decorators import (
+    during_ctf_time_only,
+    require_complete_profile,
+    require_verified_emails,
+)
+from CTFd.utils.dates import ctf_ended, ctf_paused, ctf_started
+from CTFd.utils.decorators.visibility import (
+    check_challenge_visibility,
+    check_score_visibility,
+)
+from CTFd.utils.helpers import get_infos, get_errors
+from CTFd.utils.user import authed, get_current_team, is_admin
 
 from .challenge_type import KoHChallengeType
 from .standings import get_koh_standings
@@ -29,14 +43,48 @@ def load(app):
         static_folder="assets",
     )
 
-    @koh_blueprint.route("/koh-scoreboard/<int:challenge_id>")
+    @koh_blueprint.route("/koh-scoreboard/<int:challenge_id>", methods=["GET"])
+    @check_score_visibility
     def koh_scoreboard(challenge_id):
-        standings = get_koh_standings(challenge_id)
-        return render_template('koh-scoreboard.html', standings=standings, infos=get_infos())
+        infos = get_infos()
 
-    @koh_blueprint.route("/koh-scoreboard")
+        if config.is_scoreboard_frozen():
+            infos.append("Scoreboard has been frozen")
+
+        if is_admin() is True and scores_visible() is False:
+            infos.append("Scores are not currently visible to users")
+        
+        standings = get_koh_standings(challenge_id)
+        return render_template('koh-scoreboard.html', standings=standings, infos=infos)
+
+    @koh_blueprint.route("/koh-scoreboard", methods=["GET"])
+    @require_complete_profile
+    @during_ctf_time_only
+    @require_verified_emails
+    @check_challenge_visibility
     def koh_scoreboard_index():
-        return render_template('koh-scoreboard-index.html', infos=get_infos())
+        if (
+            Configs.challenge_visibility == ChallengeVisibilityTypes.PUBLIC
+            and authed() is False
+        ):
+            pass
+        else:
+            if is_teams_mode() and get_current_team() is None:
+                return redirect(url_for("teams.private", next=request.full_path))
+
+        infos = get_infos()
+        errors = get_errors()
+
+        if ctf_started() is False:
+            errors.append(f"{Configs.ctf_name} has not started yet")
+
+        if ctf_paused() is True:
+            infos.append(f"{Configs.ctf_name} is paused")
+
+        if ctf_ended() is True:
+            infos.append(f"{Configs.ctf_name} has ended")
+
+        return render_template('koh-scoreboard-index.html', infos=infos, errors=errors)
 
     app.register_blueprint(koh_blueprint)
     register_user_page_menu_bar('KoH', '/koh-scoreboard')
